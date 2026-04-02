4783814b1c152d1f1d8ec1ea4ac5781a2067a4c8
Corentin Viel <corentin.viel@neuf.fr>
2026-04-02 17:41:36 +0200

    [MOD] upgraded VirtualHowst class, every attribute should be here now, still need to add and implement corresponding methods

--- sources/classes/VirtualHost/VirtualHost.hpp
+++ sources/classes/VirtualHost/VirtualHost.hpp
@@ -3,24 +3,76 @@
 /*                                                        :::      ::::::::   */
 /*   VirtualHost.hpp                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jweber <jweber@student.42Lyon.fr>          +#+  +:+       +#+        */
+/*   By: cviel <cviel@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/01 18:24:40 by jweber            #+#    #+#             */
-/*   Updated: 2026/04/01 19:02:27 by abetemps         ###   ########.fr       */
+/*   Updated: 2026/04/02 17:38:44 by cviel            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <map>
-#include <vector>
 #include <string>
+#include <fstream>
+#include <vector>
+#include <map>
 
 class VirtualHost
 {
 	public:
 
+		VirtualHost(void);
+		VirtualHost(VirtualHost const& other);
+		~VirtualHost();
+
+		VirtualHost&	operator=(VirtualHost const& other);
+
 	private:
-		std::vector<uint16_t>				ListeningPort;
-		std::map<std::string, Location>		Locations;
-		std::vector<std::string>			Hosts;
-		std::map<int, std::string>			ErrorPage;
+
+		enum e_reqtype
+		{
+			GET = 0,
+			POST,
+			DELETE
+		};
+
+		enum e_logtype
+		{
+			NONE = 0,
+			SUCCESS,
+			ERROR,
+			ALL
+		};
+		
+		class Location
+		{
+			public:
+
+				Location(void);
+				Location(Location const& other);
+				~Location();
+
+				Location&	operator=(Location const& other);
+			
+			private:
+
+				std::string		_root;
+				std::string		_index;
+				unsigned int	_allowedReqtypeBitfield;
+
+				enum e_logtype	_logs;
+				std::ofstream	_successLogs;
+				std::ofstream	_errorLogs;
+		};	
+	
+	
+		std::vector<std::string>		_hosts;
+		std::string						_root;
+		std::string						_index;
+		std::map<int, std::string>		_errorPage;
+		unsigned int					_allowedReqtypeBitfield;
+
+		enum e_logtype					_logs;
+		std::ofstream					_successLogs;
+		std::ofstream					_errorLogs;
+	
+		std::map<std::string, Location>	_locations;
 };
