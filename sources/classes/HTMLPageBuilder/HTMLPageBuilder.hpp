8dd4fd24721791d0600377bbf8f7edafaf180b19
aB-temps <187414947+aB-temps@users.noreply.github.com>
2026-06-01 19:43:03 +0200

    ✨ feat(HTMLPageBuilder): started something

---
+++ sources/classes/HTMLPageBuilder/HTMLPageBuilder.hpp
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   HTMLPageBuilder.hpp                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abetemps <abetemps@student.42lyon.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/06/01 19:25:11 by abetemps          #+#    #+#             */
+/*   Updated: 2026/06/01 19:29:45 by abetemps         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef		__HTMLPAGEBUILDER_HPP__
+# define	__HTMLPAGEBUILDER_HPP__
+
+# include "HTTPStatus.cpp"
+
+class	HTMLPageBuilder
+{
+	// pipe ou memfd_create(char *name, uint flags)
+};
+
+#endif
