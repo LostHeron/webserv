b0c92969ac498b60ea32abaf0008f4ca4045489c
jweber <jweber@student.42Lyon.fr>
2026-06-03 13:57:43 +0200

    build (test) second test to test location

--- test/start_test.sh
+++ test/start_test.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
+echo "interpreter = $0"
 
-echo $SHELL
+########################## TEST 1 ##################################
 echo "TEST 1: Host selection"
 CONFIG_FILE="\"host\":[
 {
@@ -29,20 +30,47 @@ WEBSERV_PID=$!
 
 #echo "sending following request"
 #echo -n "GET /index_a.html HTTP/1.1\r\nhost:host_a\r\n\r\n"
-echo -ne "GET /index_a.html HTTP/1.1\r\nhost:host_a\r\n\r\n" | nc localhost 4343 > log_req_a
+echo -ne "GET /index_a.html HTTP/1.1\r\nhost:host_a\r\n\r\n" | nc localhost 4343 > log_req_a.log
 
 #echo "sending following request"
 #echo -n "GET /index_b.html HTTP/1.1\r\nhost:host_b\r\n\r\n"
-echo -ne "GET /index_b.html HTTP/1.1\r\nhost:host_b\r\n\r\n" | nc localhost 4343 > log_req_b
+echo -ne "GET /index_b.html HTTP/1.1\r\nhost:host_b\r\n\r\n" | nc localhost 4343 > log_req_b.log
 
-LAST_LINE_A=$(tail -1 log_req_a)
-LAST_LINE_B=$(tail -1 log_req_b)
+LAST_LINE_A=$(tail -1 log_req_a.log)
+LAST_LINE_B=$(tail -1 log_req_b.log)
 
 if [ "$LAST_LINE_A" != "in a" ] || [ "$LAST_LINE_B" != "in b" ]; then
 	echo "FAILED basic host selection test";
 else
 	echo "SUCCESS";
 fi
-	
 
 kill -INT $WEBSERV_PID
+rm -rf config_file.json
+rm -rf $HOME/goinfre/tmp/
+echo
+echo
+
+########################## TEST 2 ##################################
+
+echo "TEST 2: Basic location"
+CONFIG_FILE="\"host\":[\n
+{\n
+    \"listen\":4343,\n
+    \"name\": \"host_a\",\n
+    \"root\":\"$HOME/goinfre/tmp/a\",\n
+	\"location\":[\n
+		{
+		\"name\":\"/images\",\n
+		\"alias\":\"$HOME/goinfre/tmp/b/\",\n
+		},\n
+	]\n
+},\n
+]\n"
+
+echo -e $CONFIG_FILE > config_file.json
+
+mkdir -p $HOME/goinfre/tmp/a
+echo "in a" > $HOME/goinfre/tmp/a/index_a.html
+mkdir -p $HOME/goinfre/tmp/b
+echo "in b" > $HOME/goinfre/tmp/b/index_b.html
