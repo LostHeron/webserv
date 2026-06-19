3cb5f4ff34a749929afebc1d28b8ff8ea09f373a
Corentin Viel <corentin.viel@neuf.fr>
2026-06-19 17:02:53 +0200

    [MOD] removed debug messages for UriInfo

--- sources/classes/VirtualHost/VirtualHost.cpp
+++ sources/classes/VirtualHost/VirtualHost.cpp
@@ -6,7 +6,7 @@
 /*   By: cviel <cviel@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/01 18:24:40 by jweber            #+#    #+#             */
-/*   Updated: 2026/06/19 16:44:13 by cviel            ###   ########.fr       */
+/*   Updated: 2026/06/19 17:00:01 by cviel            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@
 #include "VirtualHost.hpp"
 #include "JsonObj.hpp"
 #include "config_file.hpp"
-#include <iostream>
+
 VirtualHost::VirtualHost(VirtualHost::s_config const& conf) :
 	_conf(conf)
 {
@@ -69,7 +69,6 @@ VirtualHost::UriInfo	VirtualHost::getUriInfo(std::string const& uri) const
 	std::map<std::string, Location>::const_iterator	loc_match_it;
 	UriInfo											uri_info(this->_conf);
 
-	std::cout << current.empty() << std::endl;
 	while (current.empty() == false)
 	{
 		loc_match_it = this->_conf.location.find(current);
@@ -81,10 +80,7 @@ VirtualHost::UriInfo	VirtualHost::getUriInfo(std::string const& uri) const
 		current = current.substr(0, current.find_last_of('/'));
 	}
 	if (loc_match_it != this->_conf.location.end())
-	{
-		std::cout << "loc_match_it first = " << loc_match_it->first << std::endl;
 		VirtualHost::buildUriInfo(uri, *loc_match_it, uri_info);
-	}
 	else
 		uri_info._path += uri;
 	return (uri_info);
