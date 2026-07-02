29c13072a933ba26a125ceca232e2374e3dbe598
aB-temps <187414947+aB-temps@users.noreply.github.com>
2026-07-02 17:23:52 +0200

    ✨ feat(PUT): sketch

--- sources/classes/Request/PUTReq/PUTReq.cpp
+++ sources/classes/Request/PUTReq/PUTReq.cpp
@@ -11,6 +11,7 @@
 /* ************************************************************************** */
 
 #include "PUTReq.hpp"
+#include <fstream>
 
 // Constructors/Destructor =====================================================
 // PUTReq::PUTReq(const std::string &type, const std::string &header, const std::string &body):
@@ -27,15 +28,33 @@ PUTReq::~PUTReq(void) {}
 // Member functions ============================================================
 void	PUTReq::_uploadFile(Response &resp, const std::string &upPath)
 {
-	(void)	 resp;
-	(void)	 upPath;
+	// if (this->_isWriteMode()) { throw FORBIDDEN; }
+	std::ofstream	of;
+	of.open(upPath.c_str());
+	if (!of.is_open())
+	{
+		switch (errno)
+		{
+			case(EACCES):
+				resp.setStatus(HTTPStatus::C_ERR + HTTPStatus::FORBIDDEN);
+				return;	
+			case(ENOENT):
+				resp.setStatus(HTTPStatus::C_ERR + HTTPStatus::NOT_FOUND);
+				return;	
+			default:
+				resp.setStatus(HTTPStatus::S_ERR + HTTPStatus::INTERNAL);
+				return;
+		}
+	}
+	// of << this->_body;
+	of << "This body" ;
 }
 
 void	PUTReq::_execute(Response &resp, const VirtualHost::UriInfo &uriInfo)
 {
+	# define TMP_PUT_PATH "/home/abetemps/temp_webserv_root/put/"
 	(void) uriInfo;
 
-	// if (std::string upPath = uriInfo.getUploadPath())
-	// this->_uploadFile(upPath);
-	resp.setStatus(900);
+	std::string	upPath = TMP_PUT_PATH + this->_uri;//uriInfo.getRealPath(this->_uri);
+	this->_uploadFile(resp, upPath);
 }
